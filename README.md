6568418f52ed423f756fccb9eb7e01c1d364ebf1
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2023-06-16 23:57:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Mensageria com Spring AMQP
+
+**Aplicação desenvolvida no canal da Algaworks no [vídeo](https://www.youtube.com/watch?v=SzcvuHjRJKE). Uma introdução ao RabbitMQ**
+
+Três aplicações Spring:
+- **order-service** (Api - produtor)
+- **cashback-service** (backend simples - consumidor)
+- **notification-service** (backend simples - consumidor)
+
+As três aplicações irão se comunicar via mensageria. A **order-service** irá atuar como produtor e as outras aplicações serão consumidores.
+
+**Run MySQL and RabbitMQ**
+```
+docker-compose up
+```
+
+**RabbitMQ Management**:
+```
+http://localhost:15672
+user: rabbitmq
+pass: rabbitmq
+```
+
+**Mensageria**
+
+![Mensageria](comunicacao.png)
+
+![Exchange](exchange.png)
+
+
---
+++ notification-service/src/main/java/com/example/springamqp/aula1/RabbitMQConfig.java
@@ -0,0 +1,59 @@
+package com.example.springamqp.aula1;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue queueNotification(){
+        return new Queue("orders.v1.order-created.send-notification");
+    }
+
+    @Bean
+    public Binding binding() {
+        var queue = new Queue("orders.v1.order-created.send-notification");
+        var fanoutExchange = new FanoutExchange("orders.v1.order-created");
+        return BindingBuilder.bind(queue).to(fanoutExchange);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * Para usar json na serialização das mensagens
+     * @param connectionFactory
+     * @param messageConverter
+     * @return
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
